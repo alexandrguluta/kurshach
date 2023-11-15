2727f00cce26ecae1b8a1f870eb50a87a51ae8e4
alexandr_guluta <guluta32@gmail.com>
2023-11-15 16:52:20 +0300

    before

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-
+using System.Windows.Controls;
 namespace kursach_wpf
 {
     /// <summary>
@@ -25,30 +25,38 @@ namespace kursach_wpf
         public MainWindow()
         {
             InitializeComponent();
+            string connectionString = "Data Source=usersdata.db";
+            var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = @"SELECT * FROM tasks;";
+            var Data = command.ExecuteReader();
+            Console.WriteLine(Data.();)
             DispatcherTimer LiveTime = new DispatcherTimer();
             LiveTime.Interval = TimeSpan.FromSeconds(1);
             LiveTime.Tick += timer_Tick;
             LiveTime.Start();
-            string connectionString = "Data Source=usersdata.db";
-            using var connection = new SqliteConnection(connectionString);
-            connection.Open();
-            var command = connection.CreateCommand();
-            command.CommandText =
-            @"
-                SELECT MIN(end_time) AS lowest_end_time
-                FROM tasks;
-                ";
-            object objvalid = command.ExecuteScalar();
-            if (null != objvalid)
-            { string timer_time_string = objvalid.ToString();
-                DateTime timer_time = DateTime.ParseExact(timer_time_string, "H:mm", null, System.Globalization.DateTimeStyles.None);
+            //string connectionString = "Data Source=usersdata.db";
+            //using var connection = new SqliteConnection(connectionString);
+            //connection.Open();
+            //var command = connection.CreateCommand();
+            //command.CommandText =
+            //@"
+            //    SELECT MIN(end_time) AS lowest_end_time
+            //    FROM tasks;
+            //    ";
+            //object objvalid = command.ExecuteScalar();
+            //if (null != objvalid)
+            //{ string timer_time_string = objvalid.ToString();
+            //    DateTime timer_time = DateTime.ParseExact(timer_time_string, "H:mm", null, System.Globalization.DateTimeStyles.None);
 
-                AlarmClock clock = new AlarmClock(timer_time);
-                clock.Alarm += (sender, e) => {
-                    Question timerwin = new Question();
-                    timerwin.Show();
-                };
-            }
+            //    AlarmClock clock = new AlarmClock(timer_time);
+            //    clock.Alarm += (sender, e) => {
+            //        Question timerwin = new Question();
+            //        timerwin.Show();
+            //    //};
+            //}
+            //connection.Close();
 
         }
         void timer_Tick(object sender, EventArgs e)
---
+++ Task_Def.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kursach_wpf
+
+{
+    public class Task
+        {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string TimeOverdue { get; set; }
+    }
+    }
